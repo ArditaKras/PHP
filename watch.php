5a98951c6fb4f4d5c15e3d7f836badedacef9303
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-05-08 00:01:49 +0200

    Add files via upload

---
+++ watch.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="Live a healthier, happier, more well-rested life with Crystalized Mind">
+	<meta name="keywords" content="meditation">
+	<title>CrystalizedMind | Meditation</title>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<header>
+				<a href="index.php" class="header-logo">Crystalized<span class="highlight">Mind</span></a>
+			<nav>
+				<ul class="navbar">
+					<li><a href="index.php">Home</a></li>
+				</ul>
+			</nav>
+	</header>
+
+	<section id="wallpaper">
+		<img src="./images/wat.png">
+	</section>
+
+	<section id="newsletter">
+		<div class="klasa1">
+			<h1>Subscribe to our newsletter</h1>
+			<form>
+				<input type="email" placeholder="Enter Email...">
+				<button type="submit" class="button_1">Subscribe</button>
+			</form>
+		</div>
+	</section>
+
+	<table id="videos">
+		<tr>
+			<td class="vid01"><video src="./videos/vid01.mp4" loop controls></video><b>Meditation For Inner Peace</b></td>
+			<td class="vid02"><video src="./videos/vid02.mp4" loop controls></video><b>Guided Breathing Meditation</b></td>
+			<td class="vid03"><video src="./videos/vid03.mp4" loop controls></video><b>10 Minute Mindfulness Meditation</b></td>
+		</tr>
+		<tr>
+			<td class="vid04"><video src="./videos/vid04.mp4" loop controls></video><b>7 Min Meditation to Start Your Day</b></td>
+			<td class="vid05"><video src="./videos/vid05.mp4" loop controls></video><b>Meditation | Deepening Into the Dimension of Stillness</b></td>
+			<td class="vid06"><video src="./videos/vid06.mp4" loop controls></video><b>Meditation for Anxiety</b></td>
+		</tr>
+	</table>
+
+	<footer>
+		<p>Crystalized Mind, Copyright &copy; 2019</p>
+	</footer>
+</body>
+</html>
