8915b5f60aac8206875855d85a520152ed987d71
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-05-16 11:52:33 +0200

    Update and rename watch.php to Company.php

---
+++ Company.php
@@ -0,0 +1,27 @@
+<?php include('header.php');?>
+<link rel="stylesheet" type="text/css" href="css/Company7.css">
+
+		<script>
+		$(document).ready(function(){
+		$("h2").click(function(){
+		$(this).hide();
+		});
+		});
+		$(document).ready(function(){
+		$("#button1").click(function(){
+			$("#div1").animate({right: '30px'});
+		});
+		});
+		$(document).ready(function(){
+		$("#button2").click(function(){
+		$("#div2").animate({right: '30px'});
+		});
+		});
+		$(document).ready(function(){
+		$("#button3").click(function(){
+		$("#div3").animate({right: '30px'});
+		});
+		});
+		</script>
+	</head>
+	<body>
