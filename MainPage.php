484e8e721d54553ac80e2c6a7d2d7d58c2aad485
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-05-16 11:25:02 +0200

    Update and rename index.html to MainPage.php

---
+++ MainPage.php
@@ -0,0 +1,118 @@
+		<?php include('header.php');
+			?>
+
+			<link rel="stylesheet" type="text/css" href="css/MainPage10.css">
+			<link rel="stylesheet" href="./css/styles.css">
+
+			<main>
+
+			    <div class="container">
+
+			        <div class="main-top">
+			            <nav>
+			                <ul>
+			                    <li  class="active">
+			                        <a href="MainPage.php">
+			                            HOME
+			                        </a>
+			                    </li>
+
+			                    <li>
+			                        <a href="Company.php">COMPANY</a>
+			                    </li>
+			                    <li>
+			                        <a href="Services.php">SERVICES</a>
+			                    </li>
+			                    
+			                    <li>
+                        			<a href="Staff.php">STAFF</a>
+                   				 </li>
+
+			                </ul>
+
+			            </nav>
+			        </div>
+			        <!-- ==================================2========================================= -->
+
+			        <!-- **********************************3***************************************** -->
+			        <div class="maintop1">
+
+			            <div class="maintop2">
+			                <h1 id="logo" title="Crystalized Mind">CrystalizedMind</h1>
+			                <p>
+			                    <u>Meditation</u>
+			                </p>
+			            </div>
+
+
+			        </div>
+			    </div>
+			    <section class="about-us-banner small-banner">
+			      <h4>Your Guide to Health and Happiness</h4>
+			    </section>
+			    <!--***************************************4********************************************-->
+			    <section class="e1 our-commitment text-center grey-background">
+			        <div class="container">
+			            <h1 class="main-title">𝑂𝑢𝑟 𝑐𝑜𝑚𝑚𝑖𝑡𝑚𝑒𝑛𝑡 𝑡𝑜 𝑦𝑜𝑢</h1>
+			            <div class="m1">
+			                <div class="w1 about-description">
+			                    <div class="m2">
+			                        <h3><b>Listen & Watch </b></h3>
+			                        <p class="par">As you listen to our meditation audios you will be guided step by step,
+                                        allowing you to experience meditation directly.</p>
+			                    </div>
+			                    <div class="m2">
+			                        <h3><b>Do what’s right</b></h3>
+			                        <p class="par">We're leading the governance of a new industry, developing ethical, responsible
+			                            ways to bring greater value and better opportunities to customers.</p>
+			                    </div>
+			                </div>
+			                <div class="w2 about-description">
+			                    <div class="m2">
+			                        <h3><b>Read</b></h3>
+			                        <p class="par">Visit our reading pages to learn more about meditation.
+                                    They can help prepare you for the experience of meditation
+                                    and support you in your practice.</p>
+			                    </div>
+			                    <div class="m2">
+			                        <h3><b>Products</b></h3>
+			                        <p class="par">Our guided meditations and music are available as Cds and MP3 Downloads.
+                                     View our Cds and Mp3 Downloads or look for Crystalized Mind on Amazon or Apple iTunes...</p>
+			                    </div>
+			                </div>
+			            </div>
+			            <a href="Services.php" class="btn-element">Read more about us</a>
+			        </div>
+			    </section>
+
+	<section id="boxes">
+		<table>
+			<tr id="box1">
+				<td class="cell1"><a href="https://www.youtube.com/channel/UCM7XCXnxtYJkkMN0zf0tsSw"><img src="./images/listen.png"></a></td>
+				<td class="cell2">
+					<h3>Listen & Watch</h3>
+					<p><em>As you listen to our meditation audios you will be guided step by step,<br> allowing you to experience meditation directly.<br> Click on the icon to see more!</em></p></td>
+			</tr>
+			<tr id="box2">
+				<td class="cell3">
+					<h3>Read</h3>
+					<p><em>Visit our reading pages to learn more about meditation.<br>They can help prepare you for the experience of meditation<br> and support you in your practice.<br>Click on the icon to see more!</em></p>
+				</td>
+				<td class="cell4"><a href="Services.php"><img src="./images/product.png"></a></td>
+			</tr>
+			<tr id="box3">
+				<td class="cell5"><a href="https://choosemuse.com"><img src="./images/prod.png"></a></td>
+				<td class="cell6">
+					<h3>Products</h3>
+					<p><em>Our guided meditations and music are available as Cds and MP3 Downloads.<br>View our Cds and Mp3 Downloads or look for Crystalized Mind on Amazon or Apple iTunes...</em></p>
+				</td>
+			</tr>
+		</table>
+	</section>
+			   
+			</main>
+			<script type="text/javascript" src="./javascript.js"></script>
+
+
+
+      <?php include('footer.php');?>
