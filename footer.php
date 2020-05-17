d15b2bd5d341bdf12c9c5e96622ab3a230158808
Rajmondë Shllaku <58296379+rajmondashllaku@users.noreply.github.com>
2020-05-17 13:53:28 +0200

    footer.php

---
+++ footer.php
@@ -0,0 +1,76 @@
+
+<footer>
+
+  <div class="footer">
+    <div class="widthi">
+      <div class="aboutus">
+        <p style="margin-top: auto;" class="position">ABOUT US</p>
+        <img src="Images/imag3.jpg" width="215px" height="90px" class="imgborder" >
+        <p class="p1">
+         Crystalized Mind was officially launched in 2019 as an events company, but attendees wanted to take what they learned home with them.A small team decided to make these techniques available online so more people could experience the benefits of meditation anytime, anywhere. And that blossomed into the Crystalized Mind you see today: guided meditations, animations, articles and videos, all in the distinct Crystalized Mind style.
+        </p>
+      </div>
+    </div>
+    <div class="widthi">
+      <div class="quick">
+        <p class="position">BUY OUR MEDITATION TOOLS</p>
+        <a href="https://www.amazon.com/Store-cuenco-estilo-tibetano-meditaci%C3%B3n/dp/B01A6B0ICC?ref_=fsclp_pl_dp_1" target="_blank">&raquo; Tibetan Singing Bowl Set</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/Exquisite-Tibetan-Singing-Wooden-Striker/dp/B0035Z0C3W?ref_=fsclp_pl_dp_2" target="_blank">&raquo; Exquisite Tibetan Singing Bowl Set</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/Toysmith-331-Deluxe-Zen-Garden/dp/B00005OUHD?ref_=fsclp_pl_dp_3" target="_blank">&raquo; Toysmith Deluxe Zen Garden</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/Chakra-Crystals-Abalone-Pendant-Spiritual/dp/B01MA1VVDF?ref_=fsclp_pl_dp_4" target="_blank">&raquo; Chakra Stone Set</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/Silent-Mind-Tibetan-Meditation-Mindfulness/dp/B00XEV4WYM?ref_=fsclp_pl_dp_5" target="_blank">&raquo; Tibetan Singing Bowl Set </a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/Himalayan-Glow-1002-Pink-Crystal/dp/B001892AX2?ref_=fsclp_pl_dp_6" target="_blank">&raquo; Himalayan Glow Natural Lamp</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/My-Zen-Home-Meditation-Cushion/dp/B00TBA2MD8?ref_=fsclp_pl_dp_7" target="_blank">&raquo; Home Dhyana Meditation Cushion</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/SPACECARE-Magnetic-Creativity-Children-Education/dp/B06XGSJXP6?ref_=fsclp_pl_dp_8" target="_blank">&raquo;  Magnetic Zen Sand Garden Box</a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/Meditation-Mindfulness-Spiritual-Activation-Alignment/dp/B06ZYSNVWC?ref_=fsclp_pl_dp_10" target="_blank">&raquo; Meditation & Mindfulness Spiritual Sacred </a>
+        <hr class="viza">
+        <a href="https://www.amazon.com/DharmaObjects-Carved-Prayer-Shrine-Meditation/dp/B0768N1KDL?ref_=fsclp_pl_dp_11" target="_blank">&raquo; Solid Mango Wood</a>
+        <hr class="viza">
+      </div>
+    </div>
+    <br>
+    <div class="widthi">
+      <div class="blogpost">
+        <p class="position">WHAT WE DO<p>
+          <h3 style="margin-bottom: 0px;"><b>At Crystalized Mind we see meditation as both a practice rooted in ancient history and a topic of modern science.</b></h3>
+          <address class="admin">Meditation,CrystalizedMind.com<br>
+          Monday,25th March 2020</address>
+          <p>This is why we are as equally committed to providing authentic expertise in meditation and also studying the science of meditation.</p>
+          <h3 style="margin-bottom: 0px;"><b>Science has been an integral part of the Headspace business since day one.</b></h3>
+         
+          <br> Friday,22nd  2020</address>
+          <p>We recognize that the only way to know if we are achieving our goal of improving the health and happiness of the world is to measure it.</p>
+        </div>
+      </div>
+        <?php
+        include ('email.php');
+        ?>
+      <div class="widthi cFrm">
+        <div class="contact">
+          <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <p class="position">CONTACT US</p>
+            <input type="text" name="Person" placeholder="Your name" class="name"><br>
+            <input type="text" name="Email" placeholder=" filan_fisteku@outlook.com" class="email"><br>
+            <input type="text" name="Subject" placeholder="subject" class="subject"><br>
+            <textarea type="textarea" rows="10" name="comment" placeholder="Message" class="message"></textarea><br>
+            <input type="submit" name="submit" value="SUBMIT" class="submit"><br><br>
+
+           </form>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <div class="copy">
+    <div class="container">
+      <p>&copy;Copyright Crystalized Mind's world - All rights reserved </p>
+    </div>
+  </div>
